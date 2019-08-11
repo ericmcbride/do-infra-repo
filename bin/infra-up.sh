593e3689bc8b81df4a2286800c9a7a0f970cd244
Eric McBride <eric.mcbride@toyotaconnected.com>
2019-08-11 17:04:15 -0500

    Added error raising

--- bin/infra-up.sh
+++ bin/infra-up.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+set -e
 # make sure the terraform plan is ok (check for envs, etc)
 cd $PWD/../tf && terraform init && terraform plan && terraform apply
 
@@ -8,3 +10,6 @@ OUTFILE="kube.config"
 
 echo "Grabbing k8s config from Digital Ocean $CLUSTER_ID"
 curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${TF_VAR_do_token}" "https://api.digitalocean.com/v2/kubernetes/clusters/$CLUSTER_ID/kubeconfig" > $OUTFILE
+
+# export kube.config to KUBECONFIG ENV
+export KUBECONFIG=$PWD/kube.config
