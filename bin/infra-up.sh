c1beb0526021a10bc496188b6671979797d0ab54
Eric McBride <eric.mcbride@toyotaconnected.com>
2019-08-11 17:08:25 -0500

    Updated

--- bin/infra-up.sh
+++ bin/infra-up.sh
@@ -2,14 +2,19 @@
 set -e
 
 # make sure the terraform plan is ok (check for envs, etc)
-cd $PWD/../tf && terraform init && terraform plan && terraform apply
+cd $PWD/../tf 
+
+# Terraform commands
+terraform init 
+terraform plan 
+terraform apply
 
 # this conforms to whats in our main.tf
-CLUSTER_ID=$(cd $PWD/../tf && terraform output cluster-id)
+CLUSTER_ID=$(terraform output cluster-id)
 OUTFILE="kube.config"
 
 echo "Grabbing k8s config from Digital Ocean $CLUSTER_ID"
 curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${TF_VAR_do_token}" "https://api.digitalocean.com/v2/kubernetes/clusters/$CLUSTER_ID/kubeconfig" > $OUTFILE
 
-# export kube.config to KUBECONFIG ENV
+export kube.config to KUBECONFIG ENV
 export KUBECONFIG=$PWD/kube.config
